c85ed97d19aed4a015d3c3f70278165df24fb7b2
Logico <sufian.logico@gmail.com>
2022-01-29 17:33:52 +0500

    Backend image generation function

--- backwork.php
+++ backwork.php
@@ -1,5 +1,10 @@
 <?php
 // Get the Image name, size and background
+
+$scale = $_POST['scale'];
+$count = $_POST['count'];
+
+
 $post_image = $_FILES['image']['name'];
 $post_image_size = $_FILES['image']['size'];
 $post_image_tmp = $_FILES['image']['tmp_name'];
@@ -86,15 +91,315 @@ else
 	$msgBgImg ='Please choose image';
 }
 
+$compoundImage =  imagePool($scale,$imgUrl, $bgUrl, $count);
+
 // Output int array
-$output = array(  
+$output = array(
 	'msgImage'   	=>  $msgImage,
 	'imgUrl'   	    =>  $imgUrl,
     'msgBgImg'   	=>  $msgBgImg,
     'bgUrl'   	    =>  $bgUrl,
+	'cp'			=> $compoundImage
 );  
 
 // output in JSON format and it would be accessed on AJAX call
 echo json_encode($output);
+
+
+function imagePool($scale, $srcImg, $destImg, $count){
+
+	$extBgImg = pathinfo($destImg, PATHINFO_EXTENSION);
+	
+	$extImg = pathinfo($srcImg, PATHINFO_EXTENSION);
+	
+	$wrapperWidth = 312*$scale;
+	
+	$wrapperHeight = 123*$scale;
+	
+	
+	$dstY = 40;
+	
+	// Create Transparent Image
+	
+	$imgWrapper = imagecreatetruecolor($wrapperWidth, $wrapperHeight);
+	
+	imagesavealpha($imgWrapper, true);
+	
+	$color = imagecolorallocatealpha($imgWrapper, 0, 0, 0, 127);
+	
+	imagefill($imgWrapper, 0, 0, $color);
+	
+	// imagecopymerge($imgWrapper, $image, 0, 0, 0, 0, $width, $height, 100);
+	
+	
+	// Background Image
+	
+	if(!empty($destImg)){
+	
+	switch ($extBgImg) {
+		case "jpg":
+			$bgImage = imagecreatefromjpeg($destImg);
+		  break;
+		case "jpeg":
+			$bgImage = imagecreatefromjpeg($destImg);
+		  break;
+		case "png":
+			$bgImage = imagecreatefrompng($destImg);
+		  break;
+	
+		case "svg":
+			// $image = imagecreatefrompng ( $filename );
+			$bgImage = new Imagick();
+			$bgImage->readImageBlob(file_get_contents($destImg));
+			$bgImage->setImageFormat("png24");
+			
+			$random = md5(rand()).'.png';
+	
+			$bgImage->writeImage(__DIR__.'/img/'.$random);
+	
+			$bgImage = imagecreatefrompng ( __DIR__.'/img/'.$random );
+			
+		  break;
+		default:
+			$bgImage = imagecreatefromjpeg($destImg);
+	  }
+	
+	  
+	
+	// BG Cropping
+	if($extBgImg == 'svg'){
+		list($bgWidth, $bgHeight) = getimagesize(__DIR__.'/img/'.$random);
+	}else{
+		list($bgWidth, $bgHeight) = getimagesize($destImg);
+	}
+	
+	$msgDimesion = ($bgWidth/$bgHeight) > 1.3 ? '' : 'Background aspect ratio would be 3/2'; 
+	
+	
+	// if()
+	
+	$bgr = $bgWidth / $bgHeight;
+	
+	if ($wrapperWidth/$wrapperHeight > $bgr) {
+		$newwidth = $wrapperHeight*$bgr;
+		$newheight = $wrapperHeight;
+	} else {
+		$newheight = $wrapperWidth/$bgr;
+		$newwidth = $wrapperWidth;
+	}
+	
+	// $w=312*$scale; $h=123*$scale;
+	
+	
+	
+	// End Background Image
+	
+	// imagecopy($imgWrapper, $bgImage, 0, 0, 0, 0, $wrapperWidth, $wrapperHeight);
+	
+	$dst = imagecreatetruecolor($newwidth, $newheight);
+	
+	imagecopyresampled($dst, $bgImage, 0, 0, 0, 0, $newwidth, $newheight, $bgWidth, $bgHeight);
+	
+	}
+	
+	// Image CROPING
+	
+	$filename = $srcImg;
+	$width = $height = 40 * $scale;
+	
+	switch ($extImg) {
+		case "jpg":
+			$image = imagecreatefromjpeg ( $filename );
+		  break;
+		case "jpeg":
+			$image = imagecreatefromjpeg ( $filename );
+		  break;
+		case "png":
+			$image = imagecreatefrompng ( $filename );
+		  break;
+		case "svg":
+	
+			// $image = imagecreatefrompng ( $filename );
+			$image = new Imagick();
+			$image->readImageBlob(file_get_contents($filename));
+			$image->setImageFormat("png24");
+	
+			$random = md5(rand()).'.png';
+	
+			$image->writeImage(__DIR__.'/img/'.$random);
+	
+			$image = imagecreatefrompng ( __DIR__.'/img/'.$random );
+		  break;
+		default:
+			$image = imagecreatefromjpeg ( $filename );
+	  }
+	  
+	
+	$new_image = imagecreatetruecolor ( $width, $height ); // new wigth and height
+	
+	imagealphablending($new_image , false);
+	
+	imagesavealpha($new_image , true);
+	
+	imagecopyresampled ( $new_image, $image, 0, 0, 0, 0, $width, $height, imagesx ( $image ), imagesy ( $image ) );
+	
+	$image = $new_image;
+	
+	imagecolortransparent($image, imagecolorallocate($image, 255, 0, 255));
+	
+	// create a circular mask and use it to crop the source image.
+	$mask = imagecreatetruecolor($width, $height);
+	
+	$black = imagecolorallocate($mask, 0, 0, 0);
+	
+	$magenta = imagecolorallocate($mask, 255, 0, 255);
+	
+	imagefill($mask, 0, 0, $magenta);
+	
+	$r = min($width, $height);
+	
+	imagefilledellipse($mask, ($width / 2), ($height / 2), $r, $r, $black);
+	
+	imagecolortransparent($mask, $black);
+	
+	$orange = imagecolorallocate($mask, 255, 200, 0);
+	
+	imagesetthickness($mask, 5);
+	
+	imageellipse($mask, $width/2, $height/2, $width, $height, $orange);
+	
+	imagecopymerge($image, $mask, 0, 0, 0, 0, $width, $height, 100);
+	
+	
+	
+	$img = imagecreatetruecolor($width, $height);
+	
+	imagesavealpha($img, true);
+	
+	$color = imagecolorallocatealpha($img, 0, 0, 0, 127);
+	
+	$orange = imagecolorallocate($img, 255, 200, 0);
+	
+	imagefill($img, 0, 0, $color);
+	
+	imagecopy($img, $image, 0, 0, 0, 0, $width, $height);
+	
+	// imageellipse($img, $width/2, $height/2, $width, $height, $orange);
+	
+	
+	// Image into Background Image
+	
+	if(empty($destImg)){
+	
+		$j = 1;
+	for($i=1; $i<=$count; $i++){
+	
+	
+			// $cx = $i == 0 ? $dstX : $dstX * $i;
+			// Starts from 0
+			if($scale == 1){
+			$cy = $i % 2 != 0 ? $dstY * 0.3: $dstY * 1.8;
+			}
+	
+			if($scale == 2){
+			
+				$cy = $i % 2 != 0 ? $dstY - 15 : $dstY * 3.5;
+	
+		   }
+	
+			if($scale == 3){
+			
+				$cy = $i % 2 != 0 ? $dstY: $dstY * 5;
+	
+		   }
+			
+			if( ($i % 2) != 0){
+				// if( $i == 1){
+				//     $cx = $newwidth / ($i * $count * 0.5);
+				// }else{
+	
+					$cx = 2*(($wrapperWidth / ($i * $count )) * ( $i * ($i-$j)));
+					
+					$j++;
+				   
+				// }   
+			}else{
+			   // $cx = $i % 2 != 0 ? ($w  / ($i + 1)) : ($w / $i);    
+			}
+	
+			// $cy = $i % 2 != 0 ? $h*4 : $h;
+			// $cx = $i % 2 != 0 ? ($w  / ($i + 1)) : ($w / $i);
+	
+	   
+			imagecopy($imgWrapper, $img, $cx, $cy, 0, 0, $width, $height);
+	
+		// imagecopymerge($dest, $src, $dstX, $dstY, $srcX, 0, $src_width, $src_height, $pct);
+	}
+	
+	}else{
+	
+		$j = 1;
+	for($i=1; $i<=$count; $i++){
+	
+	
+			// $cx = $i == 0 ? $dstX : $dstX * $i;
+			// Starts from 0
+			if($scale == 1){
+			$cy = $i % 2 != 0 ? $dstY * 0.3: $dstY * 1.8;
+			}
+	
+			if($scale == 2){
+			
+				$cy = $i % 2 != 0 ? $dstY - 15 : $dstY * 3.5;
+	
+		   }
+	
+			if($scale == 3){
+			
+				$cy = $i % 2 != 0 ? $dstY: $dstY * 5;
+	
+		   }
+			
+			if( ($i % 2) != 0){
+				// if( $i == 1){
+				//     $cx = $newwidth / ($i * $count * 0.5);
+				// }else{
+	
+					$cx = 2*(($newwidth / ($i * $count )) * ( $i * ($i-$j)));
+					
+					$j++;
+				   
+				// }   
+			}else{
+			   // $cx = $i % 2 != 0 ? ($w  / ($i + 1)) : ($w / $i);    
+			}
+	
+			// $cy = $i % 2 != 0 ? $h*4 : $h;
+			// $cx = $i % 2 != 0 ? ($w  / ($i + 1)) : ($w / $i);
+	
+	   
+			imagecopy($dst, $img, $cx, $cy, 0, 0, $width, $height);
+	
+		// imagecopymerge($dest, $src, $dstX, $dstY, $srcX, 0, $src_width, $src_height, $pct);
+	}
+	
+	}
+	
+	$random = md5(rand()).'.png';
+	
+	header('Content-type: image/png');
+	if(!empty($destImg)){
+		if(empty($msgDimesion)){
+			imagepng ($dst, 'img/'.$random);
+			return $random;
+		}else{
+			return $msgDimesion;
+		}
+	}else{
+		imagepng ( $imgWrapper,  'img/'.$random);
+		return $random;
+	}
+	
+	}
 
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <title>laravel</title>
     <meta charset="utf-8" />
+    <meta name="disable-extension-feature" content="read-dom" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link
       rel="stylesheet"
@@ -39,6 +40,10 @@
       width: 100%;
       height: 100%;
       object-fit: cover;
+    }
+
+    #generate{
+      visibility: hidden;
     }
   </style>
 
@@ -109,14 +114,52 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+    <!-- <script src="./dddd.js"></script> -->
     <!-- <script src="https://superal.github.io/canvas2image/canvas2image.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <script>
+
+// var hasExtension = false;
+
+// chrome.runtime.sendMessage('kbfnbcaeplbcioakkpcpgfkobkghlhen', { message: "version" },
+//     function (reply) {
+//         if (reply) {
+//             if (reply.version) {
+//                 if (reply.version >= requiredVersion) {
+//                     hasExtension = true;
+//                 }
+//             }
+//         }
+//         else {
+//           hasExtension = false;
+//         }
+//     });
+
+//     console.log('Ext----',hasExtension)
+
+// chrome.management.get("kbfnbcaeplbcioakkpcpgfkobkghlhen", function(a){console.log(a);});
+
+// function disableExtension(disabled)
+// {
+//     global.disabled = disabled;
+// }
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request.msg == "getDisabled") {
+//         sendResponse({disabled: global.disabled});
+//         return true;
+//     }
+// });
+
+
+
+
       $(document).ready(()=>{
+        
+
 
         $('.generate').on('submit', function(e){
+          e.stopPropagation();
           e.preventDefault();
 
           // Get the scale value
@@ -126,12 +169,12 @@
           let count  = $('#count').val();
 
           // styles would be applied on the of scale
-          if(scale>=2){
+         /*  if(scale>=1){
             $('.lllll').css({
               transform:`scale(${scale})`,
               margin:'10% 20%'
             })
-          }
+          } */
          
         //  Ajaax request for uploading Images
           $.ajax({
@@ -143,43 +186,65 @@
                 processData:false,
                 success:function(data){
                   let newData = JSON.parse(data) //parse into JSON
+                  
 
-                  var html = '';
-                  // <div class="img_ar"></div>
-                  // <img src="./img/flower.png" alt="" class="img_val">
-                
-                  $('.lllll').css('background-image', 'url('+newData.bgUrl+')'); //Update backgroudn URl
+                  if(newData.cp == "Background aspect ratio would be 3/2"){
+                      alert('Background aspect ratio would be 3/2');
+                  }else{
+                    var html = `<img src="img/${newData.cp}" />`;
 
-                  // Loop through images on the basis of number of counts
-                  for(let i = 0; i<count; i++){
-                    // $('.img_val').attr('src',newData.imgUrl);
-                   
-                    html += `
-                      <div class="img_ar">
-                        <img src="${newData.imgUrl}" alt="" class="img_val">
-                      </div>
-                    `;
+                    $('.lllll').html(html);
+
+                    $('#generate').attr('href', `img/${newData.cp}`);
+
+                    $('#generate').attr('download',  newData.cp);
+
+                    $('#generate').css('visibility', 'visible');
+
                   }
 
-                  // append into the element that has .lllll class name
-                  $('.lllll').html(html);
 
-                    // This libaray help us to canvas by DOM rendered DOM elements
-                      html2canvas(document.querySelector(".lllll")).then(canvas => {
-                          //  document.body.appendChild(canvas);
-                          
-                          // Get the canvas data
-                           var dataa = canvas.toDataURL('image/png');
-                          
-                          // Relplace that with Stream/ Alternative of Blob
-                           var href = dataa.replace("image/png", "image/octet-stream");
-                          
-                          //  Assign Image link to Generate Button
-                            $('#generate').attr('href', href);
 
-                            // Assign some name and download attribute to a tag that has name of Generate
-                            $('#generate').attr('download', 'imagify.png');
-                      });
+        //           // <div class="img_ar"></div>
+        //           // <img src="./img/flower.png" alt="" class="img_val">
+                
+        //           $('.lllll').css('background-image', 'url('+newData.bgUrl+')'); //Update backgroudn URl
+
+        //           // Loop through images on the basis of number of counts
+        //           for(let i = 0; i<count; i++){
+        //             // $('.img_val').attr('src',newData.imgUrl);
+                   
+        //             html += `
+        //               <div class="img_ar">
+        //                 <img src="${newData.imgUrl}" alt="" class="img_val">
+        //               </div>
+        //             `;
+        //           }
+
+        //           // append into the element that has .lllll class name
+        //           $('.lllll').html(html);
+
+        //             // This libaray help us to canvas by DOM rendered DOM elements
+        //             $(document).ready(()=>{
+        //               html2canvas(document.querySelector(".lllll"),{foreignObjectRendering:false,useCORS: true, allowTaint:false,}).then(canva => {
+        //                    document.body.appendChild(canva);
+                          
+        //                   // Get the canva data
+        //                   //  var dataa = canva.toDataURL('image/png');
+        //                   var img = canvas.toDataURL();
+        // window.open(img);
+                          
+        //                   // Relplace that with Stream/ Alternative of Blob
+        //                   //  var href = dataa.replace("image/png", "image/octet-stream");
+                          
+        //                   //  Assign Image link to Generate Button
+        //                     $('#generate').attr('href', dataa);
+
+        //                     // Assign some name and download attribute to a tag that has name of Generate
+        //                     $('#generate').attr('download', 'imagify.png');
+        //               });
+        //             })
+                      
 
                   console.log(newData)
                 }
